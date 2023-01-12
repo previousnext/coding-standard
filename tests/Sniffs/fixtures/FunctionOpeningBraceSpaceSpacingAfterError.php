308e17e7b3a06f1ed121570a180b55c6d25b0474
dpi <pro@danielph.in>
2023-01-12 18:10:57 +0800

    ✨ Add `Squiz.WhiteSpace.FunctionOpeningBraceSpace.SpacingAfter` (#13)

---
+++ tests/Sniffs/fixtures/FunctionOpeningBraceSpaceSpacingAfterError.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Sniffs\fixtures;
+
+/**
+ * The class.
+ */
+final class FunctionOpeningBraceSpaceSpacingAfterError {
+
+  /**
+   * Test method.
+   */
+  public function foo(): int {
+
+    return 1;
+  }
+
+}
