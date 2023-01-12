239edd70c4ef33c3e19101df0be769669d0fc1b5
dpi <pro@danielph.in>
2023-01-12 17:08:31 +0800

    UnusedInheritedVariablePassedToClosure

---
+++ tests/Sniffs/fixtures/UnusedInheritedVariablePassedToClosureNoError.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Sniffs\fixtures;
+
+/**
+ * The class.
+ */
+final class UnusedInheritedVariablePassedToClosureNoError {
+
+  /**
+   * Test method.
+   */
+  public function foo(): void {
+    $a = 1;
+    $b = 2;
+    $foo = static function () use (&$a, &$b): void {
+      $a++;
+      $b++;
+    };
+  }
+
+}
