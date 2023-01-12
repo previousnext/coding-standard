9fade3e0a65392275a498727dc39254660b095d0
dpi <pro@danielph.in>
2023-01-12 17:12:05 +0800

    AlphabeticallySortedUses

---
+++ tests/Sniffs/fixtures/AlphabeticallySortedUsesNoError.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Sniffs\fixtures;
+
+use Bar\Alpha;
+use Bar\Bravo;
+use Bar\Charlie;
+
+/**
+ * The class.
+ */
+final class AlphabeticallySortedUsesNoError {
+
+  use Bar\Delta;
+  use Bar\Foxtrot;
+  use Bar\Golf;
+
+}
