f97160b254c623f478e9b62f82b4dd31c124801e
dpi <pro@danielph.in>
2023-01-12 17:04:03 +0800

    FunctionClosingBrace

---
+++ tests/Sniffs/fixtures/FunctionClosingBraceSpacingBeforeCloseError.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Sniffs\fixtures;
+
+/**
+ * The class.
+ */
+final class FunctionClosingBraceSpacingBeforeCloseError {
+
+  /**
+   * Test method.
+   */
+  public function foo(): int {
+    return 1;
+
+  }
+
+}
