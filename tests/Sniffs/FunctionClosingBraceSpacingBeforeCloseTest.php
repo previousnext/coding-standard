107799aa5032f4a0b5b0a80ab5a3ccf321c8bcc1
dpi <pro@danielph.in>
2023-01-12 17:20:24 +0800

    ✨ Add `PSR2.Methods.FunctionClosingBrace.SpacingBeforeClose` (#4)

---
+++ tests/Sniffs/FunctionClosingBraceSpacingBeforeCloseTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace PreviousNext\CodingStandard\Tests\Sniffs;
+
+/**
+ * @covers \PHP_CodeSniffer\Standards\Squiz\Sniffs\WhiteSpace\FunctionOpeningBraceSpaceSniff
+ */
+final class FunctionClosingBraceSpacingBeforeCloseTest extends Base {
+
+  public function testNoError(): void {
+    $report = self::checkFile(__DIR__ . '/fixtures/FunctionClosingBraceSpacingBeforeCloseNoError.php');
+    self::assertNoSniffErrorInFile($report);
+  }
+
+  public function testError(): void {
+    $report = self::checkFile(__DIR__ . '/fixtures/FunctionClosingBraceSpacingBeforeCloseError.php');
+    self::assertSame(1, $report->getErrorCount());
+    self::assertSniffError($report, 18, sniffName: 'PSR2.Methods.FunctionClosingBrace', code: 'SpacingBeforeClose');
+  }
+
+  protected static function getSniffName(): string {
+    return 'PSR2.Methods.FunctionClosingBrace.SpacingBeforeClose';
+  }
+
+}
