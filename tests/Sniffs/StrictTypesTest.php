2aaf69572edf4a29d5ea4116ed47251cd4d6913e
dpi <pro@danielph.in>
2023-01-12 17:41:54 +0800

    ✨ Add `SlevomatCodingStandard.TypeHints.DeclareStrictTypes` (#9)

---
+++ tests/Sniffs/StrictTypesTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace PreviousNext\CodingStandard\Tests\Sniffs;
+
+use SlevomatCodingStandard\Sniffs\TypeHints\DeclareStrictTypesSniff;
+
+/**
+ * @covers \SlevomatCodingStandard\Sniffs\TypeHints\DeclareStrictTypesSniff
+ */
+final class StrictTypesTest extends Base {
+
+  public function testNoError(): void {
+    $report = self::checkFile(__DIR__ . '/fixtures/StrictTypesNoError.php');
+    self::assertNoSniffErrorInFile($report);
+  }
+
+  public function testMissing(): void {
+    $report = self::checkFile(__DIR__ . '/fixtures/StrictTypesMissing.php');
+    self::assertSame(1, $report->getErrorCount());
+    self::assertSniffError($report, 1, DeclareStrictTypesSniff::CODE_DECLARE_STRICT_TYPES_MISSING);
+  }
+
+  public function testSpacing(): void {
+    $report = self::checkFile(__DIR__ . '/fixtures/StrictTypesSpacing.php');
+    self::assertSame(1, $report->getErrorCount());
+    self::assertSniffError($report, 3, DeclareStrictTypesSniff::CODE_INCORRECT_STRICT_TYPES_FORMAT);
+  }
+
+  protected static function getSniffName(): string {
+    return 'SlevomatCodingStandard.TypeHints.DeclareStrictTypes';
+  }
+
+}
