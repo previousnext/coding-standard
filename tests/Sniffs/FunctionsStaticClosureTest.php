c6ef31a26755cdc166e34d8c5a022e12cb893658
dpi <pro@danielph.in>
2024-06-04 16:15:27 +0800

    ✨ Add `SlevomatCodingStandard.Functions.StaticClosure`
    Fixes #18

---
+++ tests/Sniffs/FunctionsStaticClosureTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace PreviousNext\CodingStandard\Tests\Sniffs;
+
+/**
+ * @covers \SlevomatCodingStandard\Sniffs\Functions\StaticClosureSniff
+ */
+final class FunctionsStaticClosureTest extends Base {
+
+  public function testNoError(): void {
+    $report = self::checkFile(__DIR__ . '/fixtures/FunctionsStaticClosureNoError.php');
+    self::assertNoSniffErrorInFile($report);
+  }
+
+  public function testError(): void {
+    $report = self::checkFile(__DIR__ . '/fixtures/FunctionsStaticClosureError.php');
+    self::assertSame(1, $report->getErrorCount());
+    self::assertSniffError($report, 16, sniffName: 'SlevomatCodingStandard.Functions.StaticClosure', code: 'ClosureNotStatic');
+  }
+
+  protected static function getSniffName(): string {
+    return 'SlevomatCodingStandard.Functions.StaticClosure';
+  }
+
+}
---
+++ tests/Sniffs/fixtures/FunctionsStaticClosureError.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace PreviousNext\CodingStandard\Tests\Sniffs\fixtures;
+
+/**
+ * The class.
+ */
+final class FunctionsStaticClosureError {
+
+  /**
+   * Foo.
+   */
+  public function foo(): void {
+    $a = function (): void {
+      $a = 1;
+    };
+  }
+
+}
